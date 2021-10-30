7bd8a90de9a02efcdc9a12c542f3ca56cfd5c957
entcher <49116032+entcher@users.noreply.github.com>
2021-10-30 10:35:24 +0300

    add LateInYear

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,8 +1,17 @@
 #include <ctime>
+#include <assert.h>
 
 bool IsLeapYear(int year)
 {
 	return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+}
+
+int LaterInYear(int d1, int d2)
+{
+	if (d1 > d2)
+		return d1;
+	else
+		return d2;
 }
 
 int main()
