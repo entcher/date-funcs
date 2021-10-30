4e58dbb6fa2a0e2c89f799e44d541bd847a20680
entcher <49116032+entcher@users.noreply.github.com>
2021-10-30 10:45:26 +0300

    separated funcs from main

---
+++ Project1/DateFuncs.h
@@ -0,0 +1,6 @@
+#pragma once
+
+bool IsLeapYear(int year);
+int LaterInYear(int d1, int d2);
+int DaysInYear(int year);
+int DaysInYearRange(int year1, int year2);
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,34 +1,7 @@
 #include <ctime>
 #include <assert.h>
+#include "DateFuncs.h"
 
-bool IsLeapYear(int year)
-{
-	return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-}
-
-int LaterInYear(int d1, int d2)
-{
-	if (d1 > d2)
-		return d1;
-	else
-		return d2;
-}
-
-int DaysInYear(int year)
-{
-	if (IsLeapYear(year))
-		return 366;
-	else
-		return 365;
-}
-
-int DaysInYearRange(int year1, int year2)
-{
-	int sum = 0;
-	for (int i = year1; i <= year2; i++)
-		sum += DaysInYear(i);
-	return sum
-}
 
 int main()
 {
