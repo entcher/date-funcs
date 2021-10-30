cb2ef98ee6ef85a5039349508704a84563a31f67
entcher <49116032+entcher@users.noreply.github.com>
2021-10-30 10:51:22 +0300

    add tests

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,5 +5,15 @@
 
 int main()
 {
+	assert(IsLeapYear(400) == true);
+	assert(IsLeapYear(2021) == false);
 
+	assert(LaterInYear(12, 13) == 13);
+	assert(LaterInYear(13, 12) == 13);
+
+	assert(DaysInYear(400) == 366);
+	assert(DaysInYear(125) == 365);
+
+	assert(DaysInYearRange(1, 2) == 365 + 365);
+	assert(DaysInYearRange(400, 401) == 366 + 365);
 }
